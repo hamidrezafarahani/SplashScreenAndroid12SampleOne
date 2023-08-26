c75ca79dce3af557e9219876ee99df3bc03a09b8
HamidReza Farahani <h.farahani>
2023-08-26 12:25:00 +0430

    using view model and delay 5 second and then display main activity

--- app/src/main/java/com/example/splashscreenandroid12sampleone/MainActivity.kt
+++ app/src/main/java/com/example/splashscreenandroid12sampleone/MainActivity.kt
@@ -3,44 +3,42 @@ package com.example.splashscreenandroid12sampleone
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.splashscreenandroid12sampleone.ui.theme.SplashScreenAndroid12SampleOneTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            setKeepOnScreenCondition { 
+                viewModel.isLoading.value
+            }
+        }
         setContent {
             SplashScreenAndroid12SampleOneTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+                Box(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    contentAlignment = Alignment.Center
                 ) {
-                    Greeting("Android")
+                    Text(
+                        text = "Splash Screen Sample",
+                        style = MaterialTheme.typography.titleMedium,
+                        textAlign = TextAlign.Center
+                    )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SplashScreenAndroid12SampleOneTheme {
-        Greeting("Android")
     }
 }
